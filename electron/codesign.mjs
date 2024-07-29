522565073d504c67a4a210a46278f78c4539983b
Nicolas PUZIN <end3rbyte@outlook.com>
2024-07-29 18:33:01 +0200

    Bugfix/2 (#33)
    fix(#2): Non trusted application warning (windows)

--- electron/codesign.mjs
+++ electron/codesign.mjs
@@ -4,11 +4,14 @@ import { exec } from "child_process";
 
 const execPromise = promisify(exec);
 const OwnerName = process.env.CERT_OWNERNAME;
+const TimeStampServer = 'http://time.certum.pl/';
 
 export const SignTool = async (config) => {
   if (OwnerName) {
     console.info(`Signing ${config.path} to ${OwnerName}`);
-    return await execPromise(`signtool sign /fd sha256 /n "${OwnerName}" "${config.path}"`);
+    const cmd = `signtool sign /fd sha256 /tr "${TimeStampServer}" /td sha256 /n "${OwnerName}" "${config.path}"`;
+    console.info(cmd);
+    return await execPromise(cmd);
   } else {
     console.info('code signing skipped');
   }
